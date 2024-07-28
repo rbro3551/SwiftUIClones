7c03dc8d7d3151e826db652e446e6d8c0d09028f
Riley B <30447344+rbro3551@users.noreply.github.com>
2024-07-27 21:43:20 -0500

    Initial Commit

---
+++ SwiftUIClones/SwiftUIClonesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIClonesApp.swift
+//  SwiftUIClones
+//
+//  Created by Riley Brookins on 7/27/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIClonesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
