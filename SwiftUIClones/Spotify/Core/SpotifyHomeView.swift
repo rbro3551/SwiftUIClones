8b76974dddda87e10abec22c38fdcce619b55405
Riley B <30447344+rbro3551@users.noreply.github.com>
2024-08-01 23:11:14 -0500

    ImageRowCells

--- SwiftUIClones/Spotify/Core/SpotifyHomeView.swift
+++ SwiftUIClones/Spotify/Core/SpotifyHomeView.swift
@@ -13,6 +13,7 @@ struct SpotifyHomeView: View {
     @State private var currentUser: User? = nil
     @State private var selectedCategory: Category? = nil
     @State private var products: [Product] = []
+    @State private var productRows: [ProductRow] = []
     
     var body: some View {
         ZStack {
@@ -25,20 +26,18 @@ struct SpotifyHomeView: View {
                         
                         VStack(spacing: 16) {
                             recentsSection
+                                .padding(.horizontal, 16)
                             
                             if let product = products.first {
                                 newReleaseSection(product: product)
+                                    .padding(.horizontal, 16)
                             }
+                            
+                            
+                            listRows
+
                         }
-                        .padding(.horizontal, 16)
                         
-                        
-                        
-                        ForEach(0..<20) { _ in
-                            Rectangle()
-                                .fill(Color.red)
-                                .frame(width: 200, height: 200)
-                        }
                     } header: {
                         header
                     }
@@ -60,6 +59,14 @@ struct SpotifyHomeView: View {
         do {
             currentUser = try await DatabaseHelper().getUsers().first
             products = try await Array(DatabaseHelper().getProducts().prefix(upTo: 8))
+            
+            var rows: [ProductRow] = []
+            let allBrands = Set(products.map { $0.brand })
+            for brand in allBrands {
+                let products = self.products.filter({ $0.brand == brand })
+                rows.append(ProductRow(title: brand?.capitalized ?? "N/A", products: self.products))
+            }
+            productRows = rows
         } catch {
             
         }
@@ -101,6 +108,9 @@ struct SpotifyHomeView: View {
         NonLazyVGrid(columns: 2, alignment: .center, spacing: 10, items: products) { product in
             if let product {
                 SpotifyRecentsCell(imageName: product.firstImage)
+                    .asButton(.press) {
+                        
+                    }
             }
         }
     }
@@ -119,6 +129,36 @@ struct SpotifyHomeView: View {
                 
             }
         )
+    }
+    
+    private var listRows: some View {
+        ForEach(productRows) { row in
+            VStack(spacing: 8) {
+                Text(row.title)
+                    .font(.title)
+                    .fontWeight(.semibold)
+                    .foregroundStyle(.spotifyWhite)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding(.horizontal, 16)
+                
+                ScrollView(.horizontal) {
+                    HStack(alignment: .top, spacing: 16) {
+                        ForEach(row.products) { product in
+                            ImageTitleRowCell(
+                                imageSize: 120,
+                                imageName: product.firstImage,
+                                title: product.title
+                            )
+                            .asButton(.press) {
+                                
+                            }
+                        }
+                    }
+                    .padding(.horizontal, 16)
+                }
+                .scrollIndicators(.hidden)
+            }
+        }
     }
 }
 
